d50e321a52b100df5e0b15ba4496d3ad73909df6
Guillaume Naval <navalguillaume@gmail.com>
2021-11-14 11:11:12 +0100

    fixes + add links

--- public/mail-script.php
+++ public/mail-script.php
@@ -1,16 +1,20 @@
 <?php
-    $myAwardSpaceEmail = "your-AwardSpace-email-goes-here";
-    $myPersonalEmail = "your-personal-email-goes-here";
+
+    $myAwardSpaceEmail = "contact@guillaumenaval.fr";
+    $myPersonalEmail = "navalguillaume@gmail.com";
     
     if(isset($_POST['submit'])) {
+        $subject = $_POST['subject'];
         $name = $_POST['name'];
         $email = $_POST['email'];
         $message = $_POST['message'];
-        $headers = "From: Contact Form <" . $myAwardSpaceEmail . ">" . "\r\n";
+        $subject = "Message de " . $name . " <" . $email .">" . "\r\n";
+        $headers = "From: " . $name . "<" . $myAwardSpaceEmail . ">" . "\r\n";
         $headers .= "Reply-To: " . $name . " <" . $email .">" . "\r\n";
         
         echo 'Your message was sent successfully!';
-        mail($myPersonalEmail, $message, $headers);
+        mail($myPersonalEmail, $subject, $message, $headers);
+        header("Location: index.html");
     } else {
         echo 'An error has occurred!';
     }
