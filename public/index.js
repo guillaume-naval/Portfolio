9add31d261163bd8a92dc43d158b0a96eb94f175
Guillaume Naval <navalguillaume@gmail.com>
2021-11-17 22:07:55 +0100

    finishing animation

--- public/index.js
+++ public/index.js
@@ -1,5 +1,40 @@
+/* HIDE LOADER */
 setTimeout(function() {
     var element = document.getElementById('loader');
     element.classList += " hidden";
   }, 3000);
 
+/* DETECT ELEMENTS FOR ANIMATION */
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
